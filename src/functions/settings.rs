99748ceb5e8e37703f1a3782f680c29a3b4e4cf1
Michael Aaron Murphy <mmstick@pm.me>
2019-10-02 15:21:04 -0600

    feat(settings): Add functions for validating a schema's existence

--- src/functions/settings.rs
+++ src/functions/settings.rs
@@ -2,6 +2,71 @@
 
 use gio::{Settings, SettingsExt};
 use glib::GString;
+use std::path::Path;
+
+const SCHEMA_PATH: &str = "/usr/share/glib-2.0/schemas/";
+
+/// Checks if a schema exists before attempting to create a `Settings` for it
+///
+/// # Notes
+///
+/// This is equivalent to
+///
+/// ```
+/// use gtk_extras::settings;
+///  
+/// let schema = "org.freedesktop.Tracker";
+/// let buf = &mut String::with_capacity(64);
+/// if let Some(settings) = settings::new_checked_with_buffer(buf, schema) {
+///     println!("settings for {} was found", schema);
+/// }
+/// ```
+pub fn new_checked(schema: &str) -> Option<Settings> {
+    new_checked_with_buffer(&mut String::with_capacity(64), schema)
+}
+
+/// Uses a pre-allocated buffer for constructing the schema path.
+pub fn new_checked_with_buffer(buffer: &mut String, schema: &str) -> Option<Settings> {
+    if schema_exists_with_buffer(buffer, schema) {
+        Some(Settings::new(schema))
+    } else {
+        None
+    }
+}
+
+/// Verifies that a schema exists
+///
+/// The default behavior of `GSettings` is to abort a program which tries to access a
+/// schema which does not exist. However, this is less than ideal in the real world,
+/// where the existing of a schema is entirely optional, so this function provides a
+/// means to validate if a schema exists in advance.
+///
+/// # Notes
+///
+/// This is equivalent to
+///
+/// ```
+/// use gtk_extras::settings;
+///
+/// let schema = "org.gnome.nautilus";
+/// let buf = &mut String::with_capacity(64)
+/// if settings::schema_exists_with_buffer(buf, schema) {
+///     println!("settings for {} exists", schema);
+/// }
+/// ```
+pub fn schema_exists(schema: &str) -> bool {
+    schema_exists_with_buffer(&mut String::with_capacity(64), schema)
+}
+
+/// Uses a pre-allocated buffer for constructing the schema path.
+pub fn schema_exists_with_buffer(buffer: &mut String, schema: &str) -> bool {
+    buffer.clear();
+    buffer.push_str(SCHEMA_PATH);
+    buffer.push_str(schema);
+    buffer.push_str(".gschema.xml");
+
+    Path::new(buffer.as_str()).exists()
+}
 
 /// Convenience type for `org.gnome.gedit.preferences.editor`
 pub struct GeditPreferencesEditor(pub Settings);
