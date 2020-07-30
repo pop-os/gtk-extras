4544adc72dd54518729b54cb1f07470e484061c8
Ian Douglas Scott <idscott@system76.com>
2020-07-30 10:25:26 -0700

    chore!: Port from resvg to rsvg
    resvg dropped the Cairo backend[1]. So it is no longer suitable.
    
    This also fixes the build, which for some reason was failing in a resvg
    dependency.
    
    BREAKING CHANGE: API changes, since it used types from resvg
    
    [1]: https://github.com/RazrFalcon/resvg/commit/8262079a7814a450891520133f74841ef74ebabb

--- src/widgets_/svg_image.rs
+++ src/widgets_/svg_image.rs
@@ -1,26 +1,32 @@
+use cairo::Rectangle;
+use gio::{self, MemoryInputStream};
+use glib::Bytes;
 use gtk::prelude::*;
-use resvg::{backend_cairo, usvg, Options, ScreenSize};
+use librsvg::{CairoRenderer, Loader, LoadingError, SvgHandle};
 use std::path::Path;
 
-/// Renders SVG images into a GTK DrawingArea via resvg
+/// Renders SVG images into a GTK DrawingArea via rsvg
 ///
-/// [resvg]: https://github.com/RazrFalcon/resvg
+/// [rsvg]: https://gitlab.gnome.org/GNOME/librsvg
 #[derive(AsRef, Deref)]
 #[as_ref]
 #[deref]
 pub struct SvgImage(gtk::DrawingArea);
 
 impl SvgImage {
-    pub fn new(tree: usvg::Tree, opt: Options) -> Self {
+    pub fn new(handle: SvgHandle) -> Self {
         let drawing_area = cascade! {
             gtk::DrawingArea::new();
             ..connect_draw(move |w, cr| {
-                let s = ScreenSize::new(
-                    w.get_allocated_width() as u32,
-                    w.get_allocated_height() as u32,
-                ).expect("failed to create ScreenSize");
+                let rect = Rectangle {
+                    x: 0.0,
+                    y: 0.0,
+                    width: w.get_allocated_width() as f64,
+                    height: w.get_allocated_height() as f64,
+                };
 
-                backend_cairo::render_to_canvas(&tree, &opt, s, cr);
+                let renderer = CairoRenderer::new(&handle);
+                renderer.render_document(cr, &rect).unwrap();
                 Inhibit(false)
             });
         };
@@ -29,19 +35,20 @@ impl SvgImage {
     }
 
     /// Renders a SVG image which is already stored in-memory.
-    pub fn from_bytes(image: &[u8]) -> Result<Self, usvg::Error> {
-        let opt = Options::default();
-        let tree = usvg::Tree::from_data(image, &opt.usvg)?;
-
-        Ok(Self::new(tree, opt))
+    pub fn from_bytes(image: &[u8]) -> Result<Self, LoadingError> {
+        // Is there a way to avoid either cloning or unsafe cast to 'static?
+        let bytes = Bytes::from_owned(image.to_owned());
+        let stream = MemoryInputStream::new_from_bytes(&bytes);
+        let handle = Loader::new().read_stream(&stream,
+                                               None::<&gio::File>,
+                                               None::<&gio::Cancellable>)?;
+        Ok(Self::new(handle))
     }
 
     /// Renders a SVG image found at the given path.
-    pub fn from_file<P: AsRef<Path>>(image: P) -> Result<Self, usvg::Error> {
-        let opt = Options::default();
-        let tree = usvg::Tree::from_file(image, &opt.usvg)?;
-
-        Ok(Self::new(tree, opt))
+    pub fn from_file<P: AsRef<Path>>(image: P) -> Result<Self, LoadingError> {
+        let handle = Loader::new().read_path(image)?;
+        Ok(Self::new(handle))
     }
 }
 
