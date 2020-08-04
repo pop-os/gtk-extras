89f9b6e9366c2644eb06ae21cd1911915dbffd65
Ian Douglas Scott <idscott@system76.com>
2020-08-04 08:54:38 -0700

    feat: Make svg support an optional feature
    Since svg parsing requires support for css, xml, font rendering, etc.,
    rsvg pulls in a lot of dependencies. It's probably best that it's
    optional.

--- src/widgets_/mod.rs
+++ src/widgets_/mod.rs
@@ -1,5 +1,6 @@
 mod image_selection;
 mod revealing_button;
+#[cfg(feature = "svg")]
 mod svg_image;
 mod uuid_entry;
 mod variant_toggler;
@@ -7,10 +8,12 @@ mod variant_toggler;
 pub use self::{
     image_selection::{ImageSelection, SelectionVariant},
     revealing_button::RevealingButton,
-    svg_image::SvgImage,
     uuid_entry::UuidEntry,
     variant_toggler::{ToggleVariant, VariantToggler},
 };
+
+#[cfg(feature = "svg")]
+pub use self::svg_image::SvgImage;
 
 use gtk::prelude::*;
 
